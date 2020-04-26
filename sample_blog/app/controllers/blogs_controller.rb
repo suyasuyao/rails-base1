c2e8fbe068006b1ae743b4f96428ad3861ef41a2
yasuyasu-ot <y.otsubo.dss@gmail.com>
2020-04-27 02:11:56 +0900

    #4 新規作成画面の作成 (new,createアクション)

--- sample_blog/app/controllers/blogs_controller.rb
+++ sample_blog/app/controllers/blogs_controller.rb
@@ -1,2 +1,13 @@
 class BlogsController < ApplicationController
+  def index
+  
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    Blog.create(title: params[:blog][:title], content: params[:blog][:content])
+  end
 end
