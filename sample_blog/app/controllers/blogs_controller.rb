4ae0c2158333be06ef46d986dd55bef8dd8f03ee
yasuyasu-ot <y.otsubo.dss@gmail.com>
2020-04-29 15:30:56 +0900

    #4 処理の共通化

--- sample_blog/app/controllers/blogs_controller.rb
+++ sample_blog/app/controllers/blogs_controller.rb
@@ -1,4 +1,5 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update]
   def index
     @blogs = Blog.all
   end
@@ -19,15 +20,12 @@ class BlogsController < ApplicationController
   end
 
   def show
-    @blog = Blog.find(params[:id])
   end
 
   def edit
-    @blog = Blog.find(params[:id])
   end
 
   def update
-    @blog = Blog.find(params[:id])
     if @blog.update(blog_params)
       redirect_to blogs_path, notice: "ブログを編集しました！"
     else
@@ -39,5 +37,9 @@ class BlogsController < ApplicationController
 
   def blog_params
     params.require(:blog).permit(:title, :content)
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
