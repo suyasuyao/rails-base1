a008599a6c78db490ef0fb50108ddaf318c653fb
yasuyasu-ot <y.otsubo.dss@gmail.com>
2020-04-29 15:15:16 +0900

    #4 編集画面の作成

--- sample_blog/app/controllers/blogs_controller.rb
+++ sample_blog/app/controllers/blogs_controller.rb
@@ -22,15 +22,18 @@ class BlogsController < ApplicationController
     @blog = Blog.find(params[:id])
   end
 
-  # def edit
-  #   @blog = Blog.find(params[:id])
-  # end
+  def edit
+    @blog = Blog.find(params[:id])
+  end
 
-  # def update
-  #   @blog = Blog.find(params[:id])
-  #   @blog.update(blog_params)
-  #   redirect_to blogs_path
-  # end
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
+  end
 
   private
 
